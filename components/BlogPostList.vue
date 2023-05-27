5dd07eaa5b74093b0dc2ed5289949d97899ee0ef
Chris Bishop <chris@bishoptech.io>
2023-05-27 15:03:21 -0400

    Initial commit

---
+++ components/BlogPostList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data: blogPostList } = useAsyncData('blogPostList', () => {
+  return queryContent('/blog').find()
+})
+</script>
+
+<template>
+ <pre>{{ blogPostList }}</pre>
+</template>
+
+<style scoped>
+
+</style>
---
+++ content/blog/first.md
@@ -0,0 +1,13 @@
+---
+title: First Post
+description: This is my first blog post
+---
+
+# Title #1
+
+This is my first blog post. I hope you like it!
+
+```python
+def hello():
+    print("Hello, world!")
+```
